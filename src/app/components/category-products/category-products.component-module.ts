44e97e8e1830bc5025be17aa32e15b6efa1a1644
Kamkoz <kamkoz93@gmail.com>
2023-01-31 21:49:39 +0100

    feat: removed old imports

--- src/app/components/category-products/category-products.component-module.ts
+++ src/app/components/category-products/category-products.component-module.ts
@@ -2,22 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CategoryProductsComponent } from './category-products.component';
-import { MatListModule } from '@angular/material/list';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule,
-    MatListModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatAutocompleteModule,
-  ],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   declarations: [CategoryProductsComponent],
   providers: [],
   exports: [CategoryProductsComponent],
